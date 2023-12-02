481563e75762220dd8afe12c4d20f6e2482fb6e7
Abdullah <dola.doit@gmail.com>
2023-12-02 11:20:40 +0700

    use laravel ui, migration database, model relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +43,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function comment():HasMany{
+      return $this->hasMany(Comment::class);
+    }
+    public function post():HasMany{
+      return $this->hasMany(Post::class);
+    }
+    public function like():HasMany{
+      return $this->hasMany(Like::class);
+    }
 }
